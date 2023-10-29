23bda84eec75ea7c9691fe46a14fcf299a70c759
Solomiia28 <142502435+Solomiia28@users.noreply.github.com>
2023-10-29 21:07:38 +0200

    add fail js

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+const makeTransaction(quantity, pricePerDroid);
+const quantity = 5;
+const pricePerDroid = 3000;
+const totalPrice = 5 * 3000;
+const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`
+
+const quantity = 3;
+const pricePerDroid = 1000;
+const totalPrice = 3 * 1000;
+const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`
+
+const quantity = 10;
+const pricePerDroid = 500;
+const totalPrice = 10 * 500;
+const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`
---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+function getShippingMessage(country, price, deliveryFee) {
+    console.log("Shipping to Australia will cost 170 credits");
+}
+getShippingMessage("Australia", 120, 50);
+ country = "Australia";
+ price = 120;
+ deliveryFee = 50;
+const totalPrice = 120 + 50;
+const getShippingMessage = `Shipping to ${country} will cost ${totalPrice} credits`;
+  
+
+const getShippingMessage(country, price, deliveryFee);
+const country = "Germany";
+const price = 80;
+const deliveryFee = 20;
+const totalPrice = 80 + 20;
+const getShippingMessage = `Shipping to ${country} will cost ${totalPrice} credits`; 
+
+const getShippingMessage(country, price, deliveryFee);
+const country = "Sweden";
+const price = 100;
+const deliveryFee = 20;
+const totalPrice = 100 + 20;
+const getShippingMessage = `Shipping to ${country} will cost ${totalPrice} credits`; 
